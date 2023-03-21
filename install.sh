72ad397b1e95f2a6a4303d6008ceabb610b0f857
Grey Goo <greygoo@hno3.eu>
2023-03-21 23:48:14 +0000

    Add install script and ansible receipts to configure systems

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+RELEASE=$1
+LINUXFAMILY=$2
+BOARD=$3
+BUILD_DESKTOP=$4
+
+Main() {
+        InstallAnsible
+        RunAnsiblePlaybooks
+}
+
+InstallAnsible()
+{
+        echo "Installing ansible"
+        yes | DEBIAN_FRONTEND=noninteractive apt-get -yqq install ansible
+        apt clean
+} # InstallAdvancedDesktop
+
+RunAnsiblePlaybooks()
+{
+        echo "Running playbooks"
+        ansible-playbook /tmp/overlay/ansible/playbooks/*
+}
+
+Main "$@"
